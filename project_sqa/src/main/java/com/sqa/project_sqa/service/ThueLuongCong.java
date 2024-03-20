b4ed337ad458aa20714d8420024c3c653a484d1d
batchanhdao <lea81807@gmail.com>
2024-03-20 09:36:52 +0700

    tính tiền thuế TNCN bằng kiẻu dữ liệu: BigDecimal -> return String

--- project_sqa/src/main/java/com/sqa/project_sqa/service/ThueLuongCong.java
+++ project_sqa/src/main/java/com/sqa/project_sqa/service/ThueLuongCong.java
@@ -1,71 +1,83 @@
 package com.sqa.project_sqa.service;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
 //tính theo tháng
 public class ThueLuongCong {
 
-	private final float myself = 11000000;
-	private final float social_insurance = (float) 10.5;
+	private final BigDecimal myself = new BigDecimal("11000000");
+	private final BigDecimal social_insurance = new BigDecimal("10.5");
 
 	public ThueLuongCong() {
 	}
 
-	public float getMyself() {
+	public BigDecimal getMyself() {
 		return myself;
 	}
 
-	public float getSocial_insurance() {
+	public BigDecimal getSocial_insurance() {
 		return social_insurance;
 	}
 
 	// thuế thu nhập cá nhân đối với cá nhân cư trú, ký hợp đồng lao động > 03 tháng
-	public float Tax_type1(float taxable_income, float deductible) { // thu nhập chịu thuế: taxable_income
-		float tax;
-		float assessable_income = taxable_income - deductible; // thu nhập tính thuế: assessable_income
-		if (assessable_income <= 5000000) {
-			tax = assessable_income / 100 * 5;
-		} else if (assessable_income > 5000000 && assessable_income <= 10000000) {
-			tax = (float) (assessable_income / 100 * 10) - (float) (0.25 * 1000000);
-		} else if (assessable_income > 10000000 && assessable_income <= 18000000) {
-			tax = (float) (assessable_income / 100 * 15) - (float) (0.75 * 1000000);
-		} else if (assessable_income > 18000000 && assessable_income <= 32000000) {
-			tax = (float) (assessable_income / 100 * 20) - (float) (1.65 * 1000000);
-		} else if (assessable_income > 32000000 && assessable_income <= 52000000) {
-			tax = (float) (assessable_income / 100 * 25) - (float) (3.25 * 1000000);
-		} else if (assessable_income > 52000000 && assessable_income <= 80000000) {
-			tax = (float) (assessable_income / 100 * 30) - (float) (5.85 * 1000000);
+	public static String Tax_type1(BigDecimal taxable_income, BigDecimal deductible) { // thu nhập chịu thuế:
+																							// taxable_income
+		BigDecimal tax;
+		BigDecimal assessable_income = taxable_income.subtract(deductible); // thu nhập tính thuế: assessable_income
+		if (assessable_income.compareTo(new BigDecimal("5000000")) <= 0) {
+			tax = assessable_income.multiply(new BigDecimal("0.05"));
+		} else if (assessable_income.compareTo(new BigDecimal("10000000")) <= 0) {
+			tax = assessable_income.multiply(new BigDecimal("0.1")).subtract(new BigDecimal("250000"));
+		} else if (assessable_income.compareTo(new BigDecimal("18000000")) <= 0) {
+			tax = assessable_income.multiply(new BigDecimal("0.15")).subtract(new BigDecimal("750000"));
+		} else if (assessable_income.compareTo(new BigDecimal("32000000")) <= 0) {
+			tax = assessable_income.multiply(new BigDecimal("0.2")).subtract(new BigDecimal("1650000"));
+		} else if (assessable_income.compareTo(new BigDecimal("52000000")) <= 0) {
+			tax = assessable_income.multiply(new BigDecimal("0.25")).subtract(new BigDecimal("3250000"));
+		} else if (assessable_income.compareTo(new BigDecimal("80000000")) <= 0) {
+			tax = assessable_income.multiply(new BigDecimal("0.3")).subtract(new BigDecimal("5850000"));
 		} else {
-			tax = (float) (assessable_income / 100 * 35) - (float) (9.85 * 1000000);
+			tax = assessable_income.multiply(new BigDecimal("0.35")).subtract(new BigDecimal("9850000"));
 		}
-//        System.out.println(tax);
-//        DecimalFormat decimalFormat = new DecimalFormat("#.##");
-//        String roundedTax = decimalFormat.format(tax);
-		return Math.round(tax);
+		return tax.setScale(0, RoundingMode.HALF_UP).toString();
 	}
 
-//    cá nhân không ký hợp đồng lao động hoặc ký hợp đồng lao động dưới 03 tháng
-	public float Tax_type2(float taxable_income) {
-		float tax;
-		if (taxable_income >= 2000000) {
-			tax = taxable_income / 100 * 10;
+	// Thuế thu nhập đối với cá nhân không ký hợp đồng lao động hoặc ký hợp đồng lao
+	// động dưới 03 tháng
+	public String Tax_type2(BigDecimal taxable_income) {
+		BigDecimal tax;
+		if (taxable_income.compareTo(new BigDecimal("2000000")) >= 0) {
+			tax = taxable_income.multiply(new BigDecimal("0.10"));
 		} else {
-			tax = 0;
+			tax = BigDecimal.ZERO;
 		}
-		return Math.round(tax);
+		return tax.setScale(0, RoundingMode.HALF_UP).toString(); // Làm tròn số về dạng số nguyên
 	}
 
-//    thuế thu nhập đối với cá nhân không cư trú
-	public float Tax_type3(float taxable_income) {
-		float tax;
-		tax = taxable_income / 100 * 20;
-		return Math.round(tax);
+	// thuế thu nhập đối với cá nhân không cư trú
+	public String Tax_type3(BigDecimal taxable_income) {
+		BigDecimal tax;
+		tax = taxable_income.multiply(new BigDecimal("0.20"));
+		return tax.setScale(0, RoundingMode.HALF_UP).toString();
 	}
 
-	public float Deductible(float taxable_income, int dependent_person, float benevolent) { // benevolent: khoản giảm
-																							// trừ
-		float deductible = (taxable_income / 100 * this.social_insurance) + (4400000 * dependent_person) + (this.myself)
-				+ benevolent;
-		// Định dạng để làm tròn đến 2 chữ số sau dấu thập phân
-		return Math.round(deductible);
-	}
+	// Khoản giảm trừ
+    public String Deductible(BigDecimal taxable_income, int dependent_person, BigDecimal benevolent) {
+        BigDecimal deductible = taxable_income.multiply(this.social_insurance)
+                .add(new BigDecimal("4400000").multiply(BigDecimal.valueOf(dependent_person)))
+                .add(this.myself)
+                .add(benevolent);
+        return deductible.setScale(2, RoundingMode.HALF_UP).toString(); // Làm tròn số về 2 chữ số sau dấu thập phân
+    }
+
+	// Khoản giảm trừ
+    public String Deductible(BigDecimal taxable_income, BigDecimal dependent_person, BigDecimal benevolent) {
+        BigDecimal deductible = taxable_income.multiply(this.social_insurance)
+                .add(dependent_person)
+                .add(this.myself)
+                .add(benevolent);
+        return deductible.setScale(2, RoundingMode.HALF_UP).toString(); // Làm tròn số về 2 chữ số sau dấu thập phân
+    }
 
 }
