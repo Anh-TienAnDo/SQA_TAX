7da107bfcf41769c3b46fcd8f117a1f3b321a1ea
LG-BaoPTIT <bao2002pytn@gmail.com>
2024-03-25 23:11:17 +0700

    ok1

--- project_sqa/src/main/java/com/sqa/project_sqa/entities/ThueTienLuongCong.java
+++ project_sqa/src/main/java/com/sqa/project_sqa/entities/ThueTienLuongCong.java
@@ -13,7 +13,7 @@ import java.time.LocalDate;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
-public class TKThueTienLuongCong {
+public class ThueTienLuongCong {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
