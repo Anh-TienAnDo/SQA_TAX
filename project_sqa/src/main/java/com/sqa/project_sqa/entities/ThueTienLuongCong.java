b07c5fd6c4c739be1067e55ca76435e42e24760c
aa <anhdung04102002@gmail.com>
2024-03-27 13:05:36 +0700

    v3

--- project_sqa/src/main/java/com/sqa/project_sqa/entities/ThueTienLuongCong.java
+++ project_sqa/src/main/java/com/sqa/project_sqa/entities/ThueTienLuongCong.java
@@ -1,145 +1,58 @@
 
-package com.sqa.project_sqa.entities;
+    package com.sqa.project_sqa.entities;
 
-import javax.persistence.*;
-import java.time.LocalDate;
+    import lombok.AllArgsConstructor;
+    import lombok.Getter;
+    import lombok.NoArgsConstructor;
+    import lombok.Setter;
 
-@Entity
-@Table(name = "ThueTienLuongCong")
-public class ThueTienLuongCong {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id")
-    private int id;
+    import javax.persistence.*;
+    import javax.validation.constraints.Min;
+    import java.time.LocalDate;
 
-    @Column(name = "mst")
-    private String mst;
+    @Entity
+    @Getter
+    @Setter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    @Table(name = "ThueTienLuongCong")
+    public class ThueTienLuongCong {
+        @Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+        @Column(name = "id")
+        private int id;
+        @Column(name = "mst")
+        private String mst;
+        @ManyToOne
+        @JoinColumn(name = "mst", referencedColumnName = "mst", insertable = false, updatable = false)
+        private TaxPayer taxPayer;
+//        @Min(value = 0, message = "Giá trị không hợp lệ")
+        @Column(name = "thu_nhap_chiu_thue")
+        private long thuNhapChiuThue;
+//        @Min(value = 0, message = "Giá trị không hợp lệ")
+        @Column(name = "thu_nhap_duoc_mien_giam")
+        private long thuNhapDuocMienGiam;
+//        @Min(value = 0, message = "Giá trị không hợp lệ")
+        @Column(name = "khau_tru_cho_ban_than")
+        private long khauTruChoBanThan;
+//        @Min(value = 0, message = "Giá trị không hợp lệ")
+        @Column(name = "khau_tru_nguoi_phu_thuoc")
+        private long khauTruNguoiPhuThuoc;
+//        @Min(value = 0, message = "Giá trị không hợp lệ")
+        @Column(name = "khau_tru_cho_tu_thien")
+        private long khauTruChoTuThien;
+//        @Min(value = 0, message = "Giá trị không hợp lệ")
+        @Column(name = "khau_tru_cho_bao_hiem")
+        private long khauTruChoBaoHiem;
+        @Column(name = "cu_tru")
+        private boolean cuTru = true;
 
-    @Column(name = "thu_nhap_chiu_thue")
-    private long thuNhapChiuThue;
+        @Column(name = "tong_thue_phai_nop")
+        private long tongThuePhaiNop;
 
-    @Column(name = "thu_nhap_duoc_mien_giam")
-    private long thuNhapDuocMienGiam;
+        @Column(name = "tu_ngay")
+        private LocalDate tuNgay;
 
-    @Column(name = "khau_tru_cho_ban_than")
-    private long khauTruChoBanThan;
-
-    @Column(name = "khau_tru_nguoi_phu_thuoc")
-    private long khauTruNguoiPhuThuoc;
-
-    @Column(name = "khau_tru_cho_tu_thien")
-    private long khauTruChoTuThien;
-
-    @Column(name = "khau_tru_cho_bao_hiem")
-    private long khauTruChoBaoHiem;
-
-    @Column(name = "tong_thue_phai_nop")
-    private long tongThuePhaiNop;
-
-    @Column(name = "tu_ngay")
-    private LocalDate tuNgay;
-
-    @Column(name = "den_ngay")
-    private LocalDate denNgay;
-    @OneToOne(mappedBy = "thueTienLuongCong")
-    private LoaiToKhai loaiToKhai;
-
-    public ThueTienLuongCong() {
+        @Column(name = "den_ngay")
+        private LocalDate denNgay;
     }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getMst() {
-        return mst;
-    }
-
-    public void setMst(String mst) {
-        this.mst = mst;
-    }
-
-    public long getThuNhapChiuThue() {
-        return thuNhapChiuThue;
-    }
-
-    public void setThuNhapChiuThue(long thuNhapChiuThue) {
-        this.thuNhapChiuThue = thuNhapChiuThue;
-    }
-
-    public long getThuNhapDuocMienGiam() {
-        return thuNhapDuocMienGiam;
-    }
-
-    public void setThuNhapDuocMienGiam(long thuNhapDuocMienGiam) {
-        this.thuNhapDuocMienGiam = thuNhapDuocMienGiam;
-    }
-
-    public long getKhauTruChoBanThan() {
-        return khauTruChoBanThan;
-    }
-
-    public void setKhauTruChoBanThan(long khauTruChoBanThan) {
-        this.khauTruChoBanThan = khauTruChoBanThan;
-    }
-
-    public long getKhauTruNguoiPhuThuoc() {
-        return khauTruNguoiPhuThuoc;
-    }
-
-    public void setKhauTruNguoiPhuThuoc(long khauTruNguoiPhuThuoc) {
-        this.khauTruNguoiPhuThuoc = khauTruNguoiPhuThuoc;
-    }
-
-    public long getKhauTruChoTuThien() {
-        return khauTruChoTuThien;
-    }
-
-    public void setKhauTruChoTuThien(long khauTruChoTuThien) {
-        this.khauTruChoTuThien = khauTruChoTuThien;
-    }
-
-    public long getKhauTruChoBaoHiem() {
-        return khauTruChoBaoHiem;
-    }
-
-    public void setKhauTruChoBaoHiem(long khauTruChoBaoHiem) {
-        this.khauTruChoBaoHiem = khauTruChoBaoHiem;
-    }
-
-    public long getTongThuePhaiNop() {
-        return tongThuePhaiNop;
-    }
-
-    public void setTongThuePhaiNop(long tongThuePhaiNop) {
-        this.tongThuePhaiNop = tongThuePhaiNop;
-    }
-
-    public LocalDate getTuNgay() {
-        return tuNgay;
-    }
-
-    public void setTuNgay(LocalDate tuNgay) {
-        this.tuNgay = tuNgay;
-    }
-
-    public LocalDate getDenNgay() {
-        return denNgay;
-    }
-
-    public void setDenNgay(LocalDate denNgay) {
-        this.denNgay = denNgay;
-    }
-
-    public LoaiToKhai getLoaiToKhai() {
-        return loaiToKhai;
-    }
-
-    public void setLoaiToKhai(LoaiToKhai loaiToKhai) {
-        this.loaiToKhai = loaiToKhai;
-    }
-}
