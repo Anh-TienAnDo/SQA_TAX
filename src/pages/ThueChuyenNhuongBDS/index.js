d165ec665ac3b920da45166c64066e4711bd5d7b
TranKiet14 <128589801+TranKiet14@users.noreply.github.com>
2024-05-07 17:55:01 +0700

    Update

--- src/pages/ThueChuyenNhuongBDS/index.js
+++ src/pages/ThueChuyenNhuongBDS/index.js
@@ -60,6 +60,7 @@ function ThueChuyenNhuongBDS({ loai_thue_id }) {
     if (!res.message) {
       setLoading(false);
       form.resetFields();
+      setTaxPayer(null);
       notificationApi.success({
         message: "Kê khai thành công",
         duration: 3,
