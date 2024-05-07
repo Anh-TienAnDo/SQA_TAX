b605166099a99586b9387745fac600f256ea7d29
B20DCCN471-Trần Minh Nghĩa <88278748+MinhNghiaTran01@users.noreply.github.com>
2024-05-07 22:54:13 +0700

    ver10.0

--- src/utils/FormatDateTime.js
+++ src/utils/FormatDateTime.js
@@ -1,5 +1,6 @@
 const  FormatDateTime = (date) => {
-  const day = date.getDate().toString().padStart(2, '0');
+  try {
+    const day = date.getDate().toString().padStart(2, '0');
   const month = (date.getMonth() + 1).toString().padStart(2, '0'); // Tháng trong JavaScript được đếm từ 0, do đó cần cộng thêm 1
   const year = date.getFullYear();
   const hour = date.getHours().toString().padStart(2, '0');
@@ -7,6 +8,9 @@ const  FormatDateTime = (date) => {
   const second = date.getSeconds().toString().padStart(2, '0');
 
   return `${day}-${month}-${year} ${hour}:${minute}:${second}`;
+  } catch (error) {
+    return "123"
+  }
 }
 
 export const  FormatTimeOnlyDMY = (date) => {
